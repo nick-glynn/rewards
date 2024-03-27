260a28c0fceac5b653d320ab64f29c2c8de3ad1f
Nick Glynn <nick.glynn@gmail.com>
2024-03-27 06:53:05 -0500

    Initial project files

---
+++ src/test/java/net/nickg/charter/rewards/RewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nickg.charter.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
