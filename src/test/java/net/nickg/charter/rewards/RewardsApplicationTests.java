9c2ead9f74632a8d7895e44b458c8b52402f4b54
Nick Glynn <nick.glynn@gmail.com>
2024-04-05 12:23:22 -0500

    Update RewardsApplicationTest to smoke test

--- src/test/java/net/nickg/charter/rewards/RewardsApplicationTests.java
+++ src/test/java/net/nickg/charter/rewards/RewardsApplicationTests.java
@@ -1,13 +1,21 @@
 package net.nickg.charter.rewards;
 
+import net.nickg.charter.rewards.customer.CustomerRewardsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RewardsApplicationTests {
 
+	@Autowired
+	private CustomerRewardsController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
